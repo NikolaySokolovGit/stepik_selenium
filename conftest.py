8708f6f7a07b9b872bb34b1817e0fe6812694a70
Николай Соколов <n.sokolov@owen.ru>
2020-09-23 09:38:03 +0300

    функция теста переименована

--- conftest.py
+++ conftest.py
@@ -12,7 +12,7 @@ def pytest_addoption(parser):
 def browser(request):
     language = request.config.getoption('language')
     options = Options()
-    options.add_experimental_option('prefs', {'accept_languages': language})
-    browser = webdriver.Chrome()
+    options.add_experimental_option('prefs', {'intl.accept_languages': language})
+    browser = webdriver.Chrome(options=options)
     yield browser
     browser.quit()
--- test_items.py
+++ test_items.py
@@ -1,3 +1,7 @@
-def test_button(browser):
+import time
+
+
+def test_add_to_basket_button(browser):
     browser.get('http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/')
+    time.sleep(30)
     assert browser.find_elements_by_css_selector('.btn-add-to-basket')
